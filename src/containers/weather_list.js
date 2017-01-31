a4d1111aaa4da94bcea9f934e7ec410e08a976bf
davide-ravasi <info@davideravasi.com>
2017-01-31 14:46:11 +0100

    added sparkline graph

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,46 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
-	
-	renderWeather(cityData) {
-		return (
-			<tr key={cityData.city.name}>
-				<td>{cityData.city.name}</td>
-			</tr>
-		)
-	}
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+        	<Chart data={temps} color="orange"></Chart>
+        </td>
+        <td>
+        	<Chart data={pressures} color="green"></Chart>
+        </td>
+        <td>
+        	<Chart data={humidities} color="black"></Chart>
+        </td>
+      </tr>
+    );
+  }
 
-	render() {
-		return (
-			<table className="table table-hover">
-				<thead>
-					<tr>
-						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
-					</tr>
-				</thead>
-				<tbody>
-					{this.props.weather.map(this.renderWeather)}
-				</tbody>
-			</table>
-
-		);
-	}
-} 
-
-// ES6 synthax
-function mapStateToProps( { weather } ) {
-	return { weather };
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
 }
 
-// old synthax
-// function mapStateToProps( state ) {
-// 	return { weather: state.weather };
-// }
+function mapStateToProps({ weather }) {
+  return { weather };
+}
 
-export default connect(mapStateToProps)(WeatherList)
+export default connect(mapStateToProps)(WeatherList);
+
