f2dbc7439c93f9b93bd3a2ae6e0b86ad705752d0
davide-ravasi <info@davideravasi.com>
2017-02-01 15:15:35 +0100

    added element on weather list

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -9,7 +9,11 @@ export default function(state = [], action) {
 		// ppush change instance of state
 		// concat add only
 		// is better to not manipulate state
-		return state.concat([action.payload.data]);
+		//return state.concat([action.payload.data]);
+
+		// on flattening the state array to create a new one
+		// new synthaxe
+		return [action.payload.data, ...state];
 	} 
 
 	return state;
